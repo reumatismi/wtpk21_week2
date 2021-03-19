2ad507be66873e74de61effad509db8f7f6c252d
Matti Stenvall <matti.stenvall@metropolia.fi>
2021-03-19 14:22:11 +0200

    First commit to branch

--- node_example/controllers/catController.js
+++ node_example/controllers/catController.js
@@ -3,20 +3,21 @@ const catModel = require('../models/catModel');
 
 const cats = catModel.cats;
 
-const cat_list_get = (req, res) => {
+const cat_list_get = async (req, res) => {
   console.log('nakkie', req.query)
-  if (req.query.sort == 'age') {
-    const catsSort = cats.slice().sort((catA, catB) => catA.age - catB.age);
-    res.json(catsSort);
+  if (req.query.sort === 'age') {
+    //const catsSort = cats.slice().sort((catA, catB) => catA.age - catB.age);
+    res.json({ todo: 'will do later'});
     return;
   }
+  const cats = await catModel.getAllCats();
   res.json(cats);
 };
 
 const cat_get_by_id = (req, res) => {
   console.log('get one cat by id', req.params);
 
-  res.json(cats.filter(cat => cat.id == req.params.id));
+  res.json(cats.filter(cat => cat.id === req.params.id));
 };
 
 module.exports = {
--- node_example/models/catModel.js
+++ node_example/models/catModel.js
@@ -1,4 +1,30 @@
 'use strict';
+
+const pool = require('../database/db');
+const promisePool = pool.promise();
+
+const getAllCats = async () => {
+  try {
+    // TODO: do the LEFT (or INNER) JOIN to get owner name too.
+    const [rows] = await promisePool.query('SELECT * FROM wop_cat');
+    console.log('got something back from db? ')
+    return rows;
+  } catch (e) {
+    console.error('error', e.message);
+  }
+};
+
+const getAllCatsSort = async (order) => {
+  try {
+    // TODO: do the LEFT (or INNER) JOIN to get owner name too.
+    const [rows] = await promisePool.query('SELECT * FROM wop_cat ORDER BY age');
+    return rows;
+  } catch (e) {
+    console.error('error', e.message);
+  }
+};
+
+/*
 const cats = [
   {
     id: '1',
@@ -17,7 +43,8 @@ const cats = [
     filename: 'http://placekitten.com/400/302',
   },
 ];
-
+*/
 module.exports = {
-  cats,
+  getAllCats,
+  getAllCatsSort,
 };
