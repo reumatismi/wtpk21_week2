bf9e151ff5b634d3371811b9d0cd51ede067620c
Matti Stenvall <matti.stenvall@metropolia.fi>
2021-03-19 11:44:36 +0200

    Fourth commit with lime

--- node_example/controllers/catController.js
+++ node_example/controllers/catController.js
@@ -4,7 +4,11 @@ const catModel = require('../models/catModel');
 const cats = catModel.cats;
 
 const cat_list_get = (req, res) => {
-  console.log('nakkie')
+  console.log('nakkie', req.query)
+  if (req.query.sort == 'age') {
+    const catsSort = cats.sort((catA, catB) => catA.age - catB.age);
+    res.json(catsSort);
+  }
   res.json(cats);
 };
 
