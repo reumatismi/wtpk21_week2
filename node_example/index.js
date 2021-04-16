ff4623c0edcaac3ced25f81ac2a2c8cd11740f54
Matti Stenvall <matti.stenvall@metropolia.fi>
2021-04-16 11:41:53 +0300

    assigment ready for server testing

--- node_example/index.js
+++ node_example/index.js
@@ -1,8 +1,17 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.HTTP_PORT || 3000;
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'production') {
+  require('./utils/production')(app, port);
+} else {
+  require('./utils/localhost')(app, process.env.HTTPS_PORT || 8000, port);
+}
+
 const catRouter = require('./routes/catRouter');
 const userRouter = require('./routes/userRouter')
 
@@ -18,6 +27,7 @@ app.get('/', (req, res) => {
 app.use('/cat', catRouter);
 app.use('/user', userRouter);
 
-app.listen(port, () => {
+/*app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-});
+});*/
+
